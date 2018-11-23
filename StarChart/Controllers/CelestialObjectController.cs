34fe6e090471b64c836997e8749c2cbc2d1137d0
Steve Biz <mytbbucs@gmail.com>
2018-11-23 13:15:07 -0500

    Add Cont Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -20,5 +20,47 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        IActionResult GetById (int id) {
+
+            var celestialobject = _context.CelestialObjects.Find(id);
+            if (celestialobject == null)
+                return NotFound();
+
+            celestialobject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+            return Ok(celestialobject);
+
+        }
+
+        [HttpGet("{name}",Name = "GetByName")]
+        IActionResult GetByName (string name) {
+
+            var celestialobject = _context.CelestialObjects.Where(e => e.Name == name);
+            if (celestialobject == null)
+                return NotFound();
+
+            foreach(var c in celestialobject)
+            {
+                c.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == c.Id).ToList();
+            }
+             
+            return Ok(celestialobject);
+        }
+
+        [HttpGet("",Name = "GetAll")]
+        IActionResult GetAll() {
+            var celestialobject = _context.CelestialObjects;
+            if (celestialobject == null)
+                return NotFound();
+
+            foreach (var c in celestialobject)
+            {
+                c.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == c.Id).ToList();
+            }
+
+            return Ok(celestialobject);
+
+        }
     }
 }
